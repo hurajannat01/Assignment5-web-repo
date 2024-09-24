9618ef812855fc9c30c2cd1d094c2419566ef8b0
Hurajannat01 <huraj230@gmail.com>
2024-09-25 01:30:03 +0600

    added history file

---
+++ js/dom.js
@@ -0,0 +1,6 @@
+ // Add event listener for the first "Donation" button
+        document.getElementById('donateButton').addEventListener('click', function() {
+            window.location.href = window.location.origin; // Redirects to the home page
+        } );
+
+        
