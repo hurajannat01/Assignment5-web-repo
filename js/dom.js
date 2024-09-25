8c40f0952e492c8b754a4f815d03cf8f19b7f6df
Hurajannat01 <huraj230@gmail.com>
2024-09-25 12:09:41 +0600

    assignment 5 update

--- js/dom.js
+++
@@ -1,6 +0,0 @@
- // Add event listener for the first "Donation" button
-        document.getElementById('donateButton').addEventListener('click', function() {
-            window.location.href = window.location.origin; // Redirects to the home page
-        } );
-
-        
